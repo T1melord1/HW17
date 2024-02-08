0f03cca77ff8be2c050612b59ae54fa7d922cc4b
T1melord1 <vlad.bobrenok.2001@gmail.com>
2024-02-08 12:01:34 +0300

    Добавление устройства работы год-года-лет

--- Task1/src/Main.java
+++ Task1/src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         //todo создайте класс Wine. Протестируйте его работу в этом методе.
         Wine wine = new Wine("Margo", "Morgenstern", "Russia", LocalDate.of(2020, 2, 7), "Tasty");
-        wine.wineExtract(LocalDate.of(2005,12,1));
+        wine.wineExtract(LocalDate.of(2016,12,1));
 
 
 
