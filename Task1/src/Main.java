0bc3cab483367b6ac4a1038a257c98e1bca38ec1
T1melord1 <vlad.bobrenok.2001@gmail.com>
2024-02-08 09:36:08 +0300

    test

--- Task1/src/Main.java
+++ Task1/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         //todo создайте класс Wine. Протестируйте его работу в этом методе.
+        System.out.println("Test");
     }
 }
